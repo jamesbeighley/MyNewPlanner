2fd845dfecc9ccb8e2ea541ddf0e20d3936c9c01
James Beighley <jamesbeighley@gmail.com>
2020-07-15 16:25:34 -0700

    First Commit

---
+++ MyNewPlanner/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  MyNewPlanner
+//
+//  Created by JamesBeighley on 7/15/20.
+//  Copyright © 2020 james beighley. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
